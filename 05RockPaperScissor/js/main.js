9847452e3c8f1cba3ac4cd97f3c021f5088b9a91
Bharath1910 <av.bharath@proton.me>
2023-02-19 16:57:30 +0530

    added logic

--- 05RockPaperScissor/js/main.js
+++ 05RockPaperScissor/js/main.js
@@ -9,11 +9,65 @@ const userResult = document.getElementById('user-result')
 const compResult = document.getElementById('comp-result')
 const winner = document.getElementById('winner')
 
+// Scores
+let userScore = 0
+let computerScore = 0
+
 function play(option) {
     const options = ["rock", "paper", "scissors"]
 
     const rando = Math.round(Math.random() * 10) % 3
     let computerSelection = options[rando]
+
+    switch(option) {
+        case "rock":
+            switch(computerSelection) {
+                case "rock":
+                    winner.innerText = "Tied"
+                    break;
+                
+                case "paper":
+                    winner.innerText = "Computer"
+                    break;
+                
+                case "scissors":
+                    winner.innerText = "User"
+                    break;
+            }
+            break
+        
+        case "paper":
+            switch(computerSelection) {
+                case "rock":
+                    winner.innerText = "User"
+                    break;
+                
+                case "paper":
+                    winner.innerText = "Tied"
+                    break;
+                
+                case "scissors":
+                    winner.innerText = "Computer"
+                    break;
+            }
+            break
+
+        case "scissor":
+            switch(computerSelection) {
+                case "rock":
+                    winner.innerText = "Computer"
+                    break;
+                
+                case "paper":
+                    winner.innerText = "User"
+                    break;
+                
+                case "scissors":
+                    winner.innerText = "Tied"
+                    break;
+            }
+            break
+    }
 }
 
 rock.addEventListener('click', event => {
