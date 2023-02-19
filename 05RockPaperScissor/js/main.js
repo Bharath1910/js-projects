1bbd745c04839c9b09503733eb676db321151c3a
Bharath1910 <av.bharath@proton.me>
2023-02-19 16:52:05 +0530

    added play function which selects random choice

--- 05RockPaperScissor/js/main.js
+++ 05RockPaperScissor/js/main.js
@@ -9,6 +9,13 @@ const userResult = document.getElementById('user-result')
 const compResult = document.getElementById('comp-result')
 const winner = document.getElementById('winner')
 
+function play(option) {
+    const options = ["rock", "paper", "scissors"]
+
+    const rando = Math.round(Math.random() * 10) % 3
+    let computerSelection = options[rando]
+}
+
 rock.addEventListener('click', event => {
     console.log("in rock :D");
     userResult.innerText = "Rock"
